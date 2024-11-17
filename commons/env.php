74c038dc22b6e174e5f2e93d170f52b4e17f6751
MRXL <duongbui.0312@gmail.com>
2024-11-17 10:57:03 +0700

    first commit

---
+++ commons/env.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost:3000/duan1/');
+//đường dẫn vào phầm admin
+define('BASE_URL_ADMIN'       , 'http://localhost:3000/duan1/admin');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'database_duan1');  // Tên database
+
+define('PATH_ROOT'    , __DIR__ . '/../');
---
+++ controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php 
+class HomeController{
+
+    public $modelSach;
+    
+    public function __construct(){
+        $this->modelSach = new Sach();
+    }
+
+    public function home(){
+        echo "Welcome to the home page!";
+    }
+    public function trangChu(){
+        echo "Trang chủ";
+    }
+    public function danhSachSach(){
+        // echo "Danh sách";
+        $listBook= $this->modelSach->getAllBook();
+        // var_dump($listBook);die;
+        require_once './views/listBook.php';
+    }
+}
